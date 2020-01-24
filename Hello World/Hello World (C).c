ff1adcc41f9a551b9f2c87d79c3b56c4f0c9a00f
Keviny Teixeira de Jesus <keviny.t3@gmail.com>
2020-01-24 11:51:13 -0300

    Adding Hello World in C!

---
+++ Hello World/Hello World (C).c
@@ -0,0 +1,27 @@
+/******************************************************************************
+[EN] Synthesis
+Objetivo: pintf the iconic phrase "Hello World";
+In      : no entry;
+Out     : show welcome on screen with the user name;
+
+[PT] Síntese
+Objetivo: escrever com printf a icônica frase "Hello World";
+Entrada: sem entrada;
+Saída: mostrar na tela as boas vindas com o nome do usuário;
+
+Autor: Keviny Teixeira
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <locale.h> // Required to include the Portuguese language.
+
+int main()  {
+    
+    setlocale(LC_ALL,"");
+    printf("*INICIANDO PROGRAMA EM C;");
+    printf("\n> [EN] Hello World! Keviny, welcome to C language!");
+    printf("\n> [PT] Olá Mundo! Keviny, seja bem vindo à linguagem C!");
+    return 0;
+    
+}
