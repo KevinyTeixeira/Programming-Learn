000ce44765f5b8ded83871d9d91a7778c2195cce
Keviny Teixeira de Jesus <keviny.t3@gmail.com>
2020-01-27 09:14:46 -0300

    Update Hello World (C).c
    Header correction.

--- Hello World/Hello World (C).c
+++ Hello World/Hello World (C).c
@@ -1,13 +1,13 @@
 /******************************************************************************
 [EN] Synthesis
-Objetivo: pintf the iconic phrase "Hello World";
+Objetivo: printf the iconic phrase "Hello World";
 In      : no entry;
 Out     : show welcome on screen with the user name;
 
 [PT] Síntese
 Objetivo: escrever com printf a icônica frase "Hello World";
-Entrada: sem entrada;
-Saída: mostrar na tela as boas vindas com o nome do usuário;
+Entrada : sem entrada;
+Saída   : mostrar na tela as boas vindas com o nome do usuário;
 
 Autor: Keviny Teixeira
 
