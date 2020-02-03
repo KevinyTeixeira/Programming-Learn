1cc34cb7b0e8858f2319e4cbb0e40009e34f6491
Keviny Teixeira de Jesus <keviny.t3@gmail.com>
2020-02-03 15:07:50 -0300

    Update exercise, ready to merge (part 1)

--- C/Exercises/Ex. 4 - Literary Variables.c
+++ C/Exercises/Ex. 4 - Literary Variables.c
@@ -1,8 +1,8 @@
 /******************************************************************************
 [EN] Synthesis
-Objetivo: #;
-In      : #;
-Out     : #;
+Objetivo: working with literary variables with scanf and fgets;
+In      : name;
+Out     : show names on screen using scanf and fgtes;
 
 [PT] Síntese
 Objetivo: trabalhar com variáveis literarias com scanf e fgets;
@@ -26,12 +26,11 @@ int main() {
 	printf("*INICIANDO PROGRAMA EM C;\n");
 	
 	printf("> Este programa tem como objetivo apenas praticar o uso dos comandos SCANF e FGETS.\n");
-	printf("\n> Esta atribuição será feita pelo FGETS. Insira seu nome completo: "); 
+	printf("\n> Esta atribuição será feita pelo FGETS. Insira seu nome e sobrenome: "); 
 	fgets( nome, 20, stdin); // FGETS assigning a name to the variable "name"/FGETS atribundo um nome à variável "nome"
-	printf("\n> FGETS: %s ", nome);
-	printf("> Esta atribuição será feita pelo SCANF. Insira seu nome completo: ");
+	printf("> FGETS: %s ", nome);
+	printf("\n> Esta atribuição será feita pelo SCANF. Insira somente seu nome: ");
 	scanf("%s", nome); 	fflush(stdin); // SCANF assigning a name to the variable "name"/SCANF atribundo um nome à variável "nome"
 	printf("> SCANF: %s.", nome);
 	
 }
-
