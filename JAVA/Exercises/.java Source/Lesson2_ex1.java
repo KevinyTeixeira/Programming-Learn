3159279f5d0c5cf95c88e78ec17a1e2c89b42b3d
Keviny Teixeira de Jesus <keviny.t3@gmail.com>
2020-03-11 15:43:23 -0300

    Finishing .java Lesson2_ex1.java

--- JAVA/Exercises/.java Source/Lesson2_ex1.java
+++ JAVA/Exercises/.java Source/Lesson2_ex1.java
@@ -1,69 +1,65 @@
 package lesson2;
 
-import java.util.Scanner; // Required to read a user data insertion\Necessário para ler um dado inserido pelo usuário;
+import java.util.Scanner;
 
 /******************************************************************************
 [EN] Synthesis
-Objective: receive two notes from the user and perform the arithmetic mean of these values;
-In      : grade;
-Out     : display the informed grades, the average and whether the student achieved approval or not;
+Objective: receive the input data and present it on the screen;
+In      : teacher's salary, age of a child, whether a civil servant has health insurance or not, whether a person is married (c) or single (s) and, finally, the student's name;
+Out     : display the data entered by the user on the screen;
 
 [PT] Síntese
 Objetivo: receber os dados de entrada e apresenta-los na tela;
-Entrada : salário do professor, idade de uma criança, se um servidor público tem plano de saúde ou não, se uma pessoa é casa (c) ou solteira (s) e, por fim, o nome do aluno;
+Entrada : salário do professor, idade de uma criança, se um servidor público tem plano de saúde ou não, se uma pessoa é casada (c) ou solteira (s) e, por fim, o nome do aluno;
 Saída   : apresentar na tela os dados informados pelo usuário;
 
 Autor: Keviny Teixeira
 QUESTION FROM LESSON 2, EXERCISE 1;
 *******************************************************************************/
 
-/*Crie um projeto Java no ambiente eclipse (sugestão de nome de projeto - Aula2Exercicio1).
- *  Na sequência, crie uma classe com o nome Programa. Nesta classe, você deve fazer a entrada de dados das seguintes informações: 
- *  o salário de um professor (double), a idade de uma criança (int), se um servidor público tem plano de saúde ou não (boolean), se uma pessoa é casada (c) ou solteira (s) (boolean) e, por fim, o nome de um aluno (String).
-Ao terminar as entradas de dados, você deve fazer a apresentação dos valores utilizando System.out.println(). 
-Além disso, coloque textos com valor significativo para o usuário, por exemplo: "O nome informado foi: João Silveira Neto".
-*/
 public class Lesson2_ex1 {
 	
-	public void main (String args[]) {
+	public static void main (String args[]) {
 	
-	// Statementes/Declarações
-		boolean plano_de_saude = false;
-		String nome_aluno, casado_ou_solteiro;
-		char resposta;
+		// Statementes/Declarações
+		String nome_aluno;
+		char plano_de_saude, casado_ou_solteiro;
 		int idade_crianca;
 		double sl_professor;
 		
+		// Collecting Data/Coletando Dados
 		System.out.println("Informe o nome do aluno: ");
-			nome_aluno = new Scanner(System.in).next().nextLine( );
+		nome_aluno = new Scanner(System.in).nextLine();
 		System.out.println("Informe a idade da criança: ");
-			idade_crianca = new Scanner(System.in).next().nextInt( );
+		idade_crianca = new Scanner(System.in).nextInt();
 		System.out.println("Informe o salário do professor: ");
-			sl_professor = new Scanner(System.in).next().nextDouble( );
+		sl_professor = new Scanner(System.in).nextDouble();
 		System.out.println("O aluno tem plano tem plano de saúde? (S/N)");
-			resposta = new Scanner(System.in).next().charAt(0);
-			if(resposta == 'S') plano_de_saude = true;
-		System.out.println("O aluno é casado ou solteiro? (S/N)");
-		casado_ou_solteiro = new Scanner(System.in).next().nextLine();
+		plano_de_saude = new Scanner(System.in).next().charAt(0);
+		System.out.println("O aluno é casado ou solteiro? (C/S)");
+		casado_ou_solteiro = new Scanner(System.in).next().charAt(0);
 		
+		// Printing results and ending the program/Imprimindo resultados e finalizando o programa
 		System.out.println("O nome do aluno informado foi: "+ nome_aluno);
 		System.out.println("A idade da criança informada foi: "+ idade_crianca);
 		System.out.println("O salário do professor informado foi: "+ sl_professor);
-			if(plano_de_saude == true) {
-				System.out.println("Foi informado que o aluno possui plano de saúde."); }
-			else if(plano_de_saude == false) {
+			if (plano_de_saude == 'S') {
+				System.out.println("Foi informado que o aluno possui plano de saúde."); 
+				}
+			else if (plano_de_saude == 'N') {
 					System.out.println("Foi informado que o aluno não tem plano de saúde.");
 				}
 			else {
-				System.out.println("O usuário não usou caracteres válidos para a responder.");
-			}
-			if(casado_ou_solteiro == "S") {
-				System.out.println("Foi informado que o aluno possui plano de saúde.");}
-			else if(casado_ou_solteiro == "N") {
-					System.out.println("Foi informado que o aluno não tem plano de saúde.");
+				System.out.println("O usuário não usou caracteres válidos para a responder se tem plano de saúde ou não.");
+				}
+			if(casado_ou_solteiro == 'C') {
+				System.out.println("Foi informado que o aluno é casado.");
+				}
+			else if (casado_ou_solteiro == 'S') {
+					System.out.println("Foi informado que o aluno é solteiro.");
 				}
 			else {
-				System.out.println("O usuário não usou caracteres válidos para a responder.");
+				System.out.println("O usuário não usou caracteres válidos para a responder se é casado ou não.");
 		}
 	}
 }
