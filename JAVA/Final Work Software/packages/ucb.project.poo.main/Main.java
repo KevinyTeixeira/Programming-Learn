d58a9069256e8202a4d0b81415b11dfbf8f18b05
Keviny Teixeira de Jesus <keviny.t3@gmail.com>
2020-04-02 19:09:15 -0300

    Update Main.java

--- JAVA/Final Work Software/packages/ucb.project.poo.main/Main.java
+++ JAVA/Final Work Software/packages/ucb.project.poo.main/Main.java
@@ -8,13 +8,10 @@ Objective: create a program that registers employees (principals and teachers re
 In      : name, registration, CPF, salary, position, length of service and year of admission;
 Out     : display on screen the list and data of registered employees and income taxes calculated by the system;
 
-[PT] SÌntese
-Objetivo: criar um programa que cadastre funcion·rios (diretores e professores respeitando o conceito de heranÁa e polimorfismo) e possibilite ao usu·rio executar o mÈtodo getImpostoDeRenda de um funcion·rio, ou de v·rios funcion·rios (nesse caso retornando a mÈdia e o total do imposto calculado);
-Entrada : nome, matrÌcula, cpf, sal·rio, cargo, tempo de casa e ano de admiss„o;
-SaÌda   : apresentar na tela a lista e os dados dos funcion·rios cadastrados e os impostos de renda calculados pelo sistema;
-
-Autor: Keviny Teixeira
-FINAL WORK FROM ORIENTED OBJECT PROGRAM IN UCB;
+[PT] S√≠ntese
+Objetivo: criar um programa que cadastre funcion√°rios (diretores e professores respeitando o conceito de heran√ßa e polimorfismo) e possibilite ao usu√°rio executar o m√©todo getImpostoDeRenda de um funcion√°rio, ou de v√°rios funcion√°rios (nesse caso retornando a m√©dia e o total do imposto calculado);
+Entrada : nome, matr√≠cula, cpf, sal√°rio, cargo, tempo de casa e ano de admiss√£o;
+Sa√≠da   : apresentar na tela a lista e os dados dos funcion√°rios cadastrados e os impostos de renda calculados pelo sistema;
 
 PACKAGES:
 
@@ -36,13 +33,15 @@ ucb.project.poo.methods
 ucb.project.poo.reader
 - Leitor.java | Assists in the process of reading the variables.;
 
+Autor: Keviny Teixeira
+FINAL WORK FROM ORIENTED OBJECT PROGRAM IN UCB;
 ********************************************************************************************************************************************************/
 
 public class Main {
 	public static void main (String[] args) {
 
-		Visao.initEntryData(); //Adding 3 datas to ArrayList/Adicionando 3 funcion·rios ‡ ArrayList
-		SelectionMenu.Menu(); //Open selection menu/Executa o menu de seleÁ„o
+		Visao.initEntryData(); //Adding 3 datas to ArrayList/Adicionando 3 funcion√°rios √† ArrayList
+		SelectionMenu.Menu(); //Open selection menu/Executa o menu de sele√ß√£o
 
 	}
 }
